3c5b0406847fd4a750129173f546df38645ff8c6
tatitani <tatitani@naver.com>
2023-02-14 21:00:06 +0900

    윈도우 세팅

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <spdlog/spdlog.h>
+#include <glad/glad.h> // glfw include 전에 추가
 #include <GLFW/glfw3.h>
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
+    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+    glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow* window,
+    int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key, scancode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 
 int main(int argc, const char **argv)
 {
-    SPDLOG_INFO("hello, opengl");
+    SPDLOG_INFO("Hello, OpenGL!");
 
     // glfw 라이브러리 초기화, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Initialize glfw");
@@ -14,6 +36,10 @@ int main(int argc, const char **argv)
         SPDLOG_ERROR("failed to initialize glfw: {}", description);
         return -1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
@@ -26,11 +52,32 @@ int main(int argc, const char **argv)
         return -1;
     }
 
+    glfwMakeContextCurrent(window);
+
+    // glad를 활용한 OpenGL 함수 로딩
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+
+
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version: {}", fmt::ptr(glVersion));
+
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT); // 윈도우 생성 직후에는 프레임버퍼 콜백이 호출되지 않으므로 처음에 한번 호출
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
+
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+        glClearColor(0.f, 0.1f, 0.2f, 0.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
     }
     glfwTerminate();
 
