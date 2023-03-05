e88f9f00616c2dddcc8af1d73612ca91dcd1bf8a
tatitani <tatitani@naver.com>
2023-03-05 09:31:54 +0900

    4장 - GLSL

--- src/context.cpp
+++ src/context.cpp
@@ -11,10 +11,10 @@ ContextUPtr Context::Create()
 bool Context::Init()
 {
     float vertices[] = {
-        0.5f, 0.5f, 0.0f,   // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f,  // top left
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right, red
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right, green
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,  // top left, yellow
     };
 
     // 인덱스 버퍼
@@ -24,13 +24,13 @@ bool Context::Init()
         1, 2, 3, // second triangle
     };
 
+    // VBO
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 24);
+
     // VAO
     m_vertexLayout = VertexLayout::Create();
-    // VBO
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 12);
-    
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-    
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);                 // pos
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3); // color
 
     // 인덱스 버퍼
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
@@ -39,8 +39,8 @@ bool Context::Init()
     // GL_STATIC_DRAW: the data is set only once and used many times.
     // GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
         return false;
 
@@ -52,6 +52,11 @@ bool Context::Init()
         return false;
     SPDLOG_INFO("program id: {}", m_program->Get());
 
+    // uniform 변수 설정
+    // auto loc = glGetUniformLocation(m_program->Get(), "color");
+    // m_program->Use();
+    // glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
+
     glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
 
     return true;
@@ -62,7 +67,6 @@ void Context::Render()
     glClear(GL_COLOR_BUFFER_BIT);
 
     m_program->Use();
-
     // primitive 타입, ebo내 index 개수, index 데이터형, ebo 첫 데이터의 오프셋
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
