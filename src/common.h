c37fca63de3c38ca6b0e35c2c751089058752c66
tatitani <tatitani@naver.com>
2023-03-07 00:15:03 +0900

    6장 좌표계

--- src/common.h
+++ src/common.h
@@ -6,14 +6,14 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 #include <spdlog/spdlog.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+#define CLASS_PTR(klassName)                            \
+    class klassName;                                    \
+    using klassName##UPtr = std::unique_ptr<klassName>; \
+    using klassName##Ptr = std::shared_ptr<klassName>;  \
+    using klassName##WPtr = std::weak_ptr<klassName>;
 
-#define CLASS_PTR(klassName) \
-class klassName; \
-using klassName ## UPtr = std::unique_ptr<klassName>; \
-using klassName ## Ptr = std::shared_ptr<klassName>; \
-using klassName ## WPtr = std::weak_ptr<klassName>;
-
-
-std::optional<std::string> LoadTextFile(const std::string& filename);
-
+std::optional<std::string> LoadTextFile(const std::string &filename);
--- src/context.cpp
+++ src/context.cpp
@@ -11,33 +11,60 @@ ContextUPtr Context::Create()
 
 bool Context::Init()
 {
-    //[x, y, z, r, g, b, s, t]
+    //[x, y, z, s, t]
     float vertices[] = {
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   //
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  //
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, //
-        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,  //
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,  //
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,   //
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,  //
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, //
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,  //
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,   //
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,  //
+
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,   //
+        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,  //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,  //
+
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,   //
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,  //
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+        0.5f, -0.5f, 0.5f, 0.0f, 0.0f,  //
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,  //
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,   //
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,  //
+
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, //
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,  //
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,   //
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,  //
     };
 
-    // 인덱스 버퍼
     uint32_t indices[] = {
-        // note that we start from 0!
-        0, 1, 3, // first triangle
-        1, 2, 3, // second triangle
+        0, 2, 1, 2, 0, 3,       //
+        4, 5, 6, 6, 7, 4,       //
+        8, 9, 10, 10, 11, 8,    //
+        12, 14, 13, 14, 12, 15, //
+        16, 17, 18, 18, 19, 16, //
+        20, 22, 21, 22, 20, 23, //
     };
 
     // VBO
     m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(vertices));
 
     // VAO
-    const int stride = 8;
+    const int stride = 5;
     m_vertexLayout = VertexLayout::Create();
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, 0);                 // pos
-    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, sizeof(float) * 3); // color
-    m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * stride, sizeof(float) * 6); // tex
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, 0); // pos
+    // m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, sizeof(float) * 3); // color
+    m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * stride, sizeof(float) * 3); // tex
 
     // 인덱스 버퍼
-    m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
+    m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(indices));
 
     // GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
     // GL_STATIC_DRAW: the data is set only once and used many times.
@@ -80,18 +107,56 @@ bool Context::Init()
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, m_texture2->Get());
 
-    m_program->Use();
-    glUniform1i(glGetUniformLocation(m_program->Get(), "tex"), 0);
-    glUniform1i(glGetUniformLocation(m_program->Get(), "tex2"), 1);
+    m_program->Use(); // don't forget to activate the shader before setting uniforms!
+
+    m_program->SetUniform("tex", 0);
+    m_program->SetUniform("tex2", 1);
+
+    // x축으로 -55도 회전
+    auto model = glm::rotate(glm::mat4(1.0f),
+                             glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    // 카메라는 원점으로부터 z축 방향으로 -3만큼 떨어짐
+    auto view = glm::translate(glm::mat4(1.0f),
+                               glm::vec3(0.0f, 0.0f, -3.0f));
+    // 종횡비 4:3, 세로화각 45도의 원근 투영
+    auto projection = glm::perspective(glm::radians(45.0f),
+                                       (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.01f, 10.0f);
+    auto transform = projection * view * model;
+    m_program->SetUniform("transform", transform);
 
     return true;
 }
 
 void Context::Render()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
 
     m_program->Use();
+
+    auto projection = glm::perspective(glm::radians(45.0f),
+                                       (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.01f, 20.0f);
+    auto view = glm::translate(glm::mat4(1.0f),
+                               glm::vec3(0.0f, 0.0f, -3.0f));
+
+    // auto model = glm::rotate(glm::mat4(1.0f),
+    //                          glm::radians((float)glfwGetTime() * 120.0f),
+    //                          glm::vec3(1.0f, 0.5f, 0.0f));
+    // auto transform = projection * view * model;
+    // m_program->SetUniform("transform", transform);
+
+    for (size_t i = 0; i < cubePositions.size(); i++)
+    {
+        auto &pos = cubePositions[i];
+        auto model = glm::translate(glm::mat4(1.0f), pos);
+        model = glm::rotate(model,
+                            glm::radians((float)glfwGetTime() * 120.0f + 20.0f * (float)i),
+                            glm::vec3(1.0f, 0.5f, 0.0f));
+        auto transform = projection * view * model;
+        m_program->SetUniform("transform", transform);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    }
+
     // primitive 타입, ebo내 index 개수, index 데이터형, ebo 첫 데이터의 오프셋
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 }
